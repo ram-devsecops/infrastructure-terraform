2c5b59d2d7b19405eee879a5a1ed31ed33bbbe54
Nate Clark <n8@n8io.com>
2017-07-07 08:36:38 -0400

    Add kms, dns, and graphql

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,6 @@
-# Should be ran as a terragrunt user with very limited permissions
-# ...then we read in env vars (TF_VAR_*) for high level AWS administration
+# NOTE: This script hould be ran as the terragrunt user with very limited permissions
+
+# ...then we read in env vars (TF_VAR_[ENV]_AWS_*) for the admin user to build out infrastructure
 provider "aws" {
   access_key = "${var.DEV_AWS_ACCESS_KEY_ID}"
   secret_key = "${var.DEV_AWS_SECRET_ACCESS_KEY}"
@@ -8,6 +9,7 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+# Track terragrunt state via S3
 terraform {
   backend "s3" {}
 }
@@ -20,6 +22,13 @@ module "vpc" {
 # Sensitive data KMS key
 module "kms" {
   source = "git@github.com:silverback-insights/infrastructure-terraform-modules?ref=master//init/kms-key"
+}
+
+# Internal DNS
+module "dns" {
+  source = "git@github.com:silverback-insights/infrastructure-terraform-modules?ref=master//init/dns"
+
+  vpc_id = "${module.vpc.id}"
 }
 
 # Bastion
@@ -36,9 +45,9 @@ module "bastion" {
 
 # Users
 module "users" {
-  source      = "git@github.com:silverback-insights/infrastructure-terraform-modules?ref=master//init/users"
+  source          = "git@github.com:silverback-insights/infrastructure-terraform-modules?ref=master//init/users"
 
-  environment = "${var.vpc_environment}"
+  vpc_environment = "${var.vpc_environment}" # Needed bc some users aren't created in production
 }
 
 # UI Marketing (static)
@@ -59,29 +68,45 @@ module "ui_signup" {
   cicd_user_arn = "${module.users.cicd_user_arn}"
 }
 
-# # UI app (dynamic)
-# module "ui_app" {
-#   source = "git@github.com:silverback-insights/infrastructure-terraform-modules?ref=master//services/ui-app"
-#
-#   vpc_id                    = "${module.vpc.id}"
-#   vpc_environment           = "${var.vpc_environment}"
-#   kms_key_alias             = "${module.kms.key_alias}"
-#
-#   bastion_security_group_id = "${module.bastion.security_group_id}"
-#   graphql_security_group_id = "${module.graphql.security_group_id}"
-# }
+# UI app (dynamic)
+module "ui_app" {
+  source = "git@github.com:silverback-insights/infrastructure-terraform-modules?ref=master//services/ui-app"
 
+  vpc_id                    = "${module.vpc.id}"
+  kms_key_alias             = "${module.kms.key_alias}"
+
+  bastion_security_group_id = "${module.bastion.security_group_id}"
+  graphql_security_group_id = "${module.graphql.security_group_id}"
+}
 
 # Postgres
 module "postgres" {
   source                               = "git@github.com:silverback-insights/infrastructure-terraform-modules?ref=master//data-stores/postgres"
 
   vpc_id                               = "${module.vpc.id}"
-  db_password                          = "${var.DEV_PG_DB_PASS}" # via environment variable
+  db_password                          = "${var.DEV_PG_DB_PASS}"
+  dns_zone_name                        = "${module.dns.name}"
   subnet_ids                           = ["${module.vpc.private_subnet_ids}"]
   bastion_security_group_id            = "${module.bastion.security_group_id}"
 
-  alarm_max_cpu_percentage             = 100
-  alarm_free_disk_threshold_in_bytes   = 1000000000 # 1GB
-  alarm_free_memory_threshold_in_bytes = 1000000 # 1MB
+  alarm_max_cpu_percentage             = "${var.db_alarm_max_cpu_percentage}"
+  alarm_free_disk_threshold_in_bytes   = "${var.db_alarm_min_free_disk_in_bytes}"
+  alarm_free_memory_threshold_in_bytes = "${var.db_alarm_min_free_memory_in_bytes}"
+}
+
+# GraphQL
+module "graphql" {
+  source = "git@github.com:silverback-insights/infrastructure-terraform-modules?ref=master//services/graphql"
+
+  vpc_id                     = "${module.vpc.id}"
+  availability_zones         = ["${module.vpc.availability_zones}"]
+  instance_type              = "${var.graphql_instance_type}"
+
+  subnets_private            = ["${module.vpc.private_subnet_ids}"]
+  subnets_public             = ["${module.vpc.public_subnet_ids}"]
+
+  iam_profile_name           = "${module.ui_app.iam_profile_name}"
+  bastion_security_group_id  = "${module.bastion.security_group_id}"
+  ui_app_security_group_id   = "${module.ui_app.security_group_id}"
+  postgres_security_group_id = "${module.postgres.security_group_id}"
 }
--- dev/variables.tf
+++ dev/variables.tf
@@ -1,9 +1,10 @@
-# VPC variables
+# Environment
 variable "vpc_environment" {
   description = "The environment tag. Please use one of the following: dev, tst, uat, stg, or prd."
   default     = "dev"
 }
 
+# Domains 👇
 variable "domain_cert_wildcard" {
   description = "The wildcard cert name (not FQDN)"
   default     = "*.silverbackinsights.com"
@@ -17,6 +18,29 @@ variable "domain_marketing" {
 variable "domain_signup" {
   description = "The FQDN for the signup ui"
   default     = "dev-signup.silverbackinsights.com"
+}
+# Domains ☝️
+
+# DB 👇
+variable "db_alarm_max_cpu_percentage" {
+  description = "The max cpu threshold prior to alerts"
+  default = 100
+}
+
+variable "db_alarm_min_free_disk_in_bytes" {
+  description = "The min free disk space in bytes prior to alerts"
+  default = 1000000000 # 1GB
+}
+
+variable "db_alarm_min_free_memory_in_bytes" {
+  description = "The min free memory in bytes prior to alerts"
+  default = 1000000 # 1MB
+}
+# DB ☝️
+
+variable "graphql_instance_type" {
+  description = "The AWS instance type for the graphql instances"
+  default     = "t2.micro"
 }
 
 # Environment variables 👇
